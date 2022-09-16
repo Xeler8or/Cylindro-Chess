2fa97550f25cd96d491f081539617a058c494c64
Nirav Jain <nirav@Niravs-Air.lan>
2022-09-16 11:21:25 -0700

    Rotates SkyBox

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,9 +7,14 @@ public class FollowPlayer : MonoBehaviour
     public Transform player;
     public Vector3 offset = new Vector3(0f,1f,-5f);
 
+    public Transform cylinder;
+    
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = player.position + offset;
+        var rotation_quaterion = new Quaternion(transform.rotation.x, transform.rotation.y, -cylinder.rotation.z,
+            transform.rotation.w);
+        transform.rotation = rotation_quaterion;
     }
 }
