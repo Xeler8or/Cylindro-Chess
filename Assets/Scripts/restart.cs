7657e266a0da32f01922cb2f1b2c6d0bafddae9b
Anunay Bagga <anunayrajesh.bagga2017@vitstudent.ac.in>
2022-09-19 18:26:39 -0700

    Restart Menu UI

--- Assets/Scripts/restart.cs
+++ Assets/Scripts/restart.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class restart : MonoBehaviour
 {
+   
     string scene = "SampleScene";
+    public GameObject restartScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +21,18 @@ public class restart : MonoBehaviour
     }
     void OnCollisionEnter(Collision coll)
     {
-
+        Debug.Log("Collide");
         if (coll.gameObject.tag == "Player"){
-            SceneManager.LoadScene(scene);
+            Time.timeScale = 0;
+            restartScreen.SetActive(true);
+            
         }
     }
+
+    public void replay(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(scene);
+    }
+  
+    
 }
