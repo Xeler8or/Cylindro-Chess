c09f4e4af9aff6108f558a4206e20fe3f8486754
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2022-09-22 00:35:34 -0700

    bug fix

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -8,23 +8,4 @@ public class ObstacleController : MonoBehaviour
     public Constants.Pieaces piece;
     public Constants.Color color;
     // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Gravity on Obstacles
-        //rb.AddForce(new Vector3(-transform.position.x, -transform.position.y, 0f));
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Destroy(this.gameObject);
-            //Call Analytics
-        }
-    }
 }
