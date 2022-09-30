5ef312dd6b0e0e2e9fd4f6b5c3e964d84dfa6470
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2022-09-30 00:43:46 -0700

    changed camera angle, background and cylinder color for better visibility

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset = new Vector3(0f,1f,-5f);
+    public Vector3 offset = new Vector3(10f,1f,-5f);
 
     // Update is called once per frame
     void FixedUpdate()
