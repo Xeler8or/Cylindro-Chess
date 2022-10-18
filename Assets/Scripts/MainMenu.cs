9c9dcabe1653335354761b1d41a4b1fa39ecd547
Phani Chaitanya Pendyala <ppendyal@usc.edu>
2022-10-18 10:54:56 -0700

    Chaitanya - Adding main menu and ability to switch from main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
