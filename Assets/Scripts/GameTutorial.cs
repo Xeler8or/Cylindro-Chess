6b718d8de23a659fb6cce82ab6e4af58376fc195
Phani Chaitanya Pendyala <prndyala@gmail.com>
2022-10-20 01:31:01 -0700

    Adding new tutorials.

--- Assets/Scripts/GameTutorial.cs
+++ Assets/Scripts/GameTutorial.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameTutorial : MonoBehaviour
@@ -10,10 +11,16 @@ public class GameTutorial : MonoBehaviour
     private int tutorialIndex = 0;
     private static bool showText = false;
     private string[] tutorialTexts = new string[]{ Constants.MOVE_LEFT_RIGHT, 
-                                       Constants.CHANGE_SHAPE,
-                                       Constants.COLOR_CHANGE,
-                                       Constants.AUTOMATIC_RIGHT_SECTION,
-                                       Constants.LOCK_TUTORIAL };
+                                                   Constants.CHANGE_SHAPE,
+                                                   Constants.CHANGE_TO_CUBE,
+                                                   Constants.COLOR_CHANGE,
+                                                   Constants.PASS_THROUGH_RED_PORTION,
+                                                   Constants.AUTOMATIC_RIGHT_SECTION,
+                                                   Constants.LOCK_TUTORIAL,
+                                                   Constants.ROTATE_RIGHT_360,
+                                                   Constants.ROTATE_LEFT_360,
+                                                   Constants.PERFORM_ON_LOCK
+    };
 
     // Update is called once per frame
     void Update()
@@ -27,7 +34,10 @@ public class GameTutorial : MonoBehaviour
             {
                 tutorialBackground.SetActive(false);
                 showText = false;
+                tutorialIndex++;
             }
+            else
+                tutorialIndex = tutorialIndex;
         }
         else
         {
