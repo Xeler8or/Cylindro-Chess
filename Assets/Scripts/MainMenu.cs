20c305fabfd86309f2f5e71cfa69043babae4b3e
Phani Chaitanya Pendyala <prndyala@gmail.com>
2022-11-03 18:50:21 -0700

    Made the following change: 1. Finished dividing the tutorial. 2. Added a Endless Mode. 3. Added a Levels Menu to select the level the user wants to play.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -26,39 +26,14 @@ public class MainMenu : MonoBehaviour
 
     public void LoadTutorial(string tutorialName)
     {
-        if (tutorialName == "MOVEMENT")
-        {
-            Levels.SetCurrentLevel(tutorialName);
-        }
-        else if (tutorialName == "SHAPE")
-        {
-            
-        }
-        else if (tutorialName == "COLOR")
-        {
-            
-        }
-        else if (tutorialName == "DEFAULT_LEFT")
-        {
-            
-        }
-        else if (tutorialName == "LOCK")
-        {
-            
-        }
-        else if (tutorialName == "COINS")
-        {
-            
-        }
-        else if (tutorialName == "BOUNCE")
-        {
-            
-        }
-        else if (tutorialName == "HEALTH")
-        {
-            
-        }
+        GameTutorial.startTutorial();
+        Levels.SetCurrentLevel(tutorialName);
         Time.timeScale = 1;
         SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevels()
+    {
+        SceneManager.LoadScene(4);
     }
 }
