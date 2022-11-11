e5fba4c84786030ca470195cefc4111c92347b50
Nirav <nkjain@usc.edu>
2022-11-10 16:15:13 -0800

    New Lock added

--- Assets/Scripts/LockRotator2.cs
+++ Assets/Scripts/LockRotator2.cs
@@ -4,29 +4,28 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class LockRotator : MonoBehaviour
+public class LockRotator2 : MonoBehaviour
 {
     public float rightRotation = 200f;
     public float leftRotation = 200f;
     public float rotateRight = 0f;
     public float rotateLeft = 0f;
-    private PlayerController _player;
-    public bool lock2Rotate = false;
+    private LockRotator lock1;
+    public bool lock3Rotate = false;
     public bool dir = true;
     // True == Right
     // public GameObject instructions;
 
     private void Start()
     {
-        _player = FindObjectOfType<PlayerController>();
+        lock1 = FindObjectOfType<LockRotator>();
     }
     
     
 
     void Update()
     {
-        
-        if (!_player.platformRotate)
+        if (lock1.lock2Rotate && lock1.dir)
         {
             if (Input.GetKey("d") || Input.GetKey("right"))
             {
@@ -35,30 +34,16 @@ public class LockRotator : MonoBehaviour
                 transform.Rotate(0f, leftRotation * Time.deltaTime, 0f, Space.Self);
                 rotateRight += leftRotation * Time.deltaTime;
                 // rotateLeft += leftRotation * Time.deltaTime;;
-
+                dir = true;
                 // Debug.Log("right rotations: "+rotateRight);
                 // Debug.Log("right rotations: " + (int)(rotateRight / 360.0));
-                if (!lock2Rotate)
+                if (!lock3Rotate)
                 {
                     dir = true;
                 }
             }
 
-            if (Input.GetKey("a") || Input.GetKey("left"))
-            {
-                // Rotate cylinder to right
-                // instructions.SetActive(false);
-                transform.Rotate(0f, -rightRotation * Time.deltaTime, 0f, Space.Self);
-                rotateLeft += -rightRotation * Time.deltaTime;
-                // rotateRight -= rightRotation * Time.deltaTime;;
-
-                // Debug.Log("left rotations: "+rotateLeft);
-                // Debug.Log("left rotations: " + (int)(rotateLeft / 360.0));
-                if (!lock2Rotate)
-                {
-                    dir = false;
-                }
-            }
+            
             // lock code text => 2R, 2L
 
 
@@ -69,33 +54,46 @@ public class LockRotator : MonoBehaviour
             //     instructions.SetActive(true);
             //
             // }
-            if (Input.GetKey("z"))
-            {
-                _player.ContinuePlay();
-                // instructions.SetActive(true);
-            } 
+            
         }
-        
+
+        if (lock1.lock2Rotate && !lock1.dir)
+        {
+            if (Input.GetKey("a") || Input.GetKey("left"))
+            {
+                // Rotate cylinder to right
+                // instructions.SetActive(false);
+                transform.Rotate(0f, -rightRotation * Time.deltaTime, 0f, Space.Self);
+                rotateLeft += -rightRotation * Time.deltaTime;
+                // rotateRight -= rightRotation * Time.deltaTime;;
+                dir = false;
+                // Debug.Log("left rotations: "+rotateLeft);
+                // Debug.Log("left rotations: " + (int)(rotateLeft / 360.0));
+            }
+            
+            if (!lock3Rotate)
+            {
+                dir = false;
+            }
+        }
+
         
 
     }
-
+    
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("lock2"))
+        if (other.gameObject.CompareTag("lock3"))
         {
             // Instantiate(lockRotator,
             //     new Vector3(transform.position.x, transform.position.y-5f, transform.position.z + 20f), Quaternion.Euler(new Vector3(-90f,0f,0f)));
-            lock2Rotate = true;
-            Debug.Log("Triggerred Lock 2");
-            Debug.Log(dir);
+            lock3Rotate = true;
         }
     }
-
+    
     private void OnTriggerExit(Collider other)
     {
-        lock2Rotate = false;
-        Debug.Log("Not Triggerred Lock 2");
-        Debug.Log(dir);
+        lock3Rotate = false;
     }
 }
