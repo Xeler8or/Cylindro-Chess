802185dfb8a3876b8bebb43123de01f45337f75c
nkjain-usc <nkjain@usc.edu>
2022-11-17 20:54:02 -0800

    Camera Angle on Lock fixed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,8 +7,11 @@ public class FollowPlayer : MonoBehaviour
     public Transform player;
     private Vector3 innerCylinderOffset = new Vector3(0f,5f,-10f);
     private Vector3 outerCylinderOffset = new Vector3(0f, -3f, -10f);
-    private Vector3 lockZoneOffset = new Vector3(0f, 15f, -20f);
+    private Vector3 lockZoneOffset = new Vector3(0f, 25f, 0f);
+    // private Vector3 lockZoneOffset = new Vector3(0f, 15f, -20f);
+
     private PlayerController _player;
+    private bool _cameraRotate = false;
 
     private bool once = false;
     // Update is called once per frame
@@ -26,14 +29,29 @@ public class FollowPlayer : MonoBehaviour
             }
             else
             {
-                transform.position = player.position + innerCylinderOffset;
+                if (!_player.platformRotate)
+                {
+                    transform.position = player.position + lockZoneOffset;
+                }
+                else
+                {
+                    transform.position = player.position + innerCylinderOffset;
+                }
             }
-            if (!_player.platformRotate)
+            if (!_player.platformRotate && !_cameraRotate)
             {
-                transform.position = player.position + lockZoneOffset;
-                // transform.Rotate(40f,0f,0f);
+                _cameraRotate = true;
+                transform.localEulerAngles = new Vector3(65, 3.91f, 3);
+                // transform.position = player.position + lockZoneOffset;
+                // transform.localRotation = Quaternion.Euler(3, 0, 45);
                 // once = true;
 
+            }
+
+            if (_player.platformRotate && _cameraRotate)
+            {
+                _cameraRotate = false;
+                transform.Rotate(-40f,0f,0f);
             }
 
             // if (!_player.platformRotate && !once)
